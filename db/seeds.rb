5b33577967ebf39f4bebfe5bc34c4de777dd2938
Raphael Cruz <raphacruz_90@hotmail.com>
2022-09-16 16:07:26 -0300

    Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Pet.destroy_all
+
+puts 'Creating pets...'
+rock = { name: 'Rock', category: 'Dog', birth: Date.new(2019, 06, 13), adoption: false }
+apollo = { name: 'Apollo', category: 'Dog', birth: Date.new(2021, 03, 03), adoption: false }
+atila = { name: 'Átila', category: 'Dog', birth: Date.new(2021, 11, 02), adoption: false }
+simba = { name: 'Simba', category: 'Cat', birth: Date.new(2020, 01, 25), adoption: false }
+hunter = { name: 'Hunter', category: 'Cat', birth: Date.new(2020, 12, 07), adoption: false }
+
+
+[rock, apollo, atila, simba, hunter].each do |attributes|
+  pet = Pet.create!(attributes)
+  puts "Created #{pet.name}"
+end
+puts 'Finished!'
