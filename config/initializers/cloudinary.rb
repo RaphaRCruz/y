c143921958adcc81223272b0c91d8c3927a6c0b2
Raphael Cruz <raphacruz_90@hotmail.com>
2022-09-22 12:15:35 -0300

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = ENV['rapharcruz']
+  config.api_key = ENV['257692479653283']
+  config.api_secret = ENV['38xPkughZ0wY6LZGyHmbDb3EioU']
+  config.secure = true
+end
