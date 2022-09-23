d6b896798e8fed8174c3f6108f7ddbfccb00a2cb
Raphael Cruz <raphacruz_90@hotmail.com>
2022-09-23 11:05:38 -0300

    Update

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,14 @@
 class Pet < ApplicationRecord
+  CATEGORY = [:dog, :cat, :other]
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :birth, presence: true
+
+  before_validation :capitalize
+
+  def capitalize
+    name.capitalize!
+    category.capitalize!
+  end
 end
--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV['rapharcruz']
-  config.api_key = ENV['257692479653283']
-  config.api_secret = ENV['38xPkughZ0wY6LZGyHmbDb3EioU']
+  config.cloud_name = 'rapharcruz'
+  config.api_key = '257692479653283'
+  config.api_secret = '38xPkughZ0wY6LZGyHmbDb3EioU'
   config.secure = true
 end
